46aea0320884ff31bc3ab4064efb8e4490e0a6b9
gojigoj <ghazi.ahmadf@gmail.com>
2019-07-25 21:40:19 +0700

    Add Bottom Nav and Fragment

---
+++ app/src/main/java/com/motion/takku/ProfleFragment.java
@@ -0,0 +1,22 @@
+package com.motion.takku;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProfleFragment extends Fragment {
+
+    public ProfleFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+}
