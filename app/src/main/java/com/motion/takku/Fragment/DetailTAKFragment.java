b9a2f8796ef4140a347ad5c0d005b7d47cb5d40c
gojigoj <ghazi.ahmadf@gmail.com>
2019-08-06 21:59:47 +0700

    Home Fragment (Profile)

---
+++ app/src/main/java/com/motion/takku/Fragment/DetailTAKFragment.java
@@ -0,0 +1,42 @@
+package com.motion.takku.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.motion.takku.R;
+
+public class DetailTAKFragment extends Fragment {
+    Toolbar mtoolbar;
+
+    public DetailTAKFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_detailtak, container, false);
+
+        mtoolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(mtoolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Detail Tak");
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowHomeEnabled(true);
+        mtoolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().onBackPressed();
+            }
+        });
+
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/motion/takku/Fragment/InputTAKFragment.java
@@ -0,0 +1,41 @@
+package com.motion.takku.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.motion.takku.R;
+
+public class InputTAKFragment extends Fragment {
+    Toolbar mtoolbar;
+
+    public InputTAKFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_inputtak, container, false);
+
+        mtoolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(mtoolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Edit Profile");
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowHomeEnabled(true);
+        mtoolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().onBackPressed();
+            }
+        });
+
+        return view;
+    }
+}
