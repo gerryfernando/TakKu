0592b6b46d71a94546d99e689ecba0f2e9a0a33b
boyf123 <boyfernando55@gmail.com>
2019-08-21 18:04:15 +0700

    pull

--- app/src/main/java/com/motion/takku/Fragment/EventsFragment.java
+++ app/src/main/java/com/motion/takku/Fragment/EventsFragment.java
@@ -9,12 +9,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.motion.takku.R;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class EventsFragment extends Fragment {
+
     Toolbar mtoolbar;
 
+    private int[] img =new int[] {
+            R.drawable.bglogin,R.drawable.bglogin,R.drawable.bglogin,R.drawable.bglogin,R.drawable.bglogin
+    };
+    private String[] imgTitle= new String[]{
+
+      "Test","Test","Test","Test","Test"
+
+    };
     public EventsFragment() {
     }
 
@@ -23,11 +35,21 @@ public class EventsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_events, container, false);
 
+        CarouselView carouselView =view.findViewById(R.id.carousel);
+        carouselView.setImageListener(new ImageListener() {
+            @Override
+            public void setImageForPosition(int position, ImageView imageView) {
+
+            }
+        });
+
         mtoolbar = view.findViewById(R.id.toolbar);
         ((AppCompatActivity)getActivity()).setSupportActionBar(mtoolbar);
         ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Events");
 
         return view;
     }
+
+
 
 }
