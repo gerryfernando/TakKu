f88dd4c458b0f84ee6e297fc2093b185bd83cfba
boyf123 <boyfernando55@gmail.com>
2019-08-21 18:12:05 +0700

    pull

--- app/src/main/java/com/motion/takku/Fragment/EventsFragment.java
+++ app/src/main/java/com/motion/takku/Fragment/EventsFragment.java
@@ -10,9 +10,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.motion.takku.R;
 import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageClickListener;
 import com.synnapps.carouselview.ImageListener;
 
 public class EventsFragment extends Fragment {
@@ -36,10 +38,17 @@ public class EventsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_events, container, false);
 
         CarouselView carouselView =view.findViewById(R.id.carousel);
+        carouselView.setPageCount(img.length);
         carouselView.setImageListener(new ImageListener() {
             @Override
             public void setImageForPosition(int position, ImageView imageView) {
-
+                imageView.setImageResource(img[position]);
+            }
+        });
+        carouselView.setImageClickListener(new ImageClickListener() {
+            @Override
+            public void onClick(int position) {
+                Toast.makeText(EventsFragment.this,imgTitle[position],Toast.LENGTH_SHORT).show();
             }
         });
 
